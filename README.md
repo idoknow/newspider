60417c9ea6720d4635fc682e7165521755870dc1
idoknow <hack.lliot@gmail.com>
2018-01-22 16:46:12 +0900

    경향신문 & 동아일보 크롤러 추가

---
+++ README.md
@@ -0,0 +1,18 @@
+# newspider
+
+news crawler
+
+## Currently supports
+
+서울일보
+
+* 정치
+* 프리즘
+* 사회
+* 경제
+* IT
+* 날씨
+
+중앙일보
+
+* 
--- test/test.js
+++ test/test.js
@@ -1,18 +1,34 @@
-const SeoulIlboCrawler = require('./SeoulIlboCrawler');
-const ChoongangIlboCrawler = require('./ChoongangIlboCrawler');
+const SeoulIlboCrawler = require('../lib/SeoulIlboCrawler');
+const KyeonghangCrawler = require('../lib/KyeonghangCrawler');
+const DongaCrawler = require('../lib/DongaCrawler');
+const ChoongangIlboCrawler = require('../lib/ChoongangIlboCrawler');
+const Config = require('../lib/Config');
+const newsConfig = Config['news'];
 
 const sOption = {
-	baseUrl: 'http://www.seoulilbo.com/news',
-	kind: 'S2N22',
-	pageUrlSelector: '.list-titles'
+	baseUrl: newsConfig['seoul']['baseUrl'],
+	kind: newsConfig['seoul']['kind']['economy'],
+	pageUrlSelector: newsConfig['seoul']['pageUrlSelector']
 }
 
 const cOption = {
-	baseUrl: 'http://news.joins.com',
-	kind: '/politics/diplomacy',
-	pageUrlSelector: '.headline'
+	baseUrl: newsConfig['choongang']['baseUrl'],
+	kind: newsConfig['choongang']['kind']['economy'],
+	pageUrlSelector: newsConfig['choongang']['pageUrlSelector']
 }
 
-// const testCrawler = SeoulIlboCrawler(option);
-const testCrawler = ChoongangIlboCrawler(cOption);
+const kOption = {
+	baseUrl: newsConfig['kyeonghang']['baseUrl'],
+	kind: newsConfig['kyeonghang']['kind']['society'],
+	pageUrlSelector: newsConfig['kyeonghang']['pageUrlSelector']
+}
+
+const dOption = {
+	baseUrl: newsConfig['donga']['baseUrl'],
+	kind: newsConfig['donga']['kind']['event'],
+	pageUrlSelector: newsConfig['donga']['pageUrlSelector']
+}
+
+// const testCrawler = SeoulIlboCrawler(sOption);
+const testCrawler = DongaCrawler(dOption);
 testCrawler.startCrawl();
