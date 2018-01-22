1b51b01210fb0ec429c7d8387869599a8b2495b5
idoknow <hack.lliot@gmail.com>
2018-01-22 16:57:06 +0900

    processResult랑 shouldStopCrawl을 option으로 받음

--- test/test.js
+++ test/test.js
@@ -8,25 +8,49 @@ const newsConfig = Config['news'];
 const sOption = {
 	baseUrl: newsConfig['seoul']['baseUrl'],
 	kind: newsConfig['seoul']['kind']['economy'],
-	pageUrlSelector: newsConfig['seoul']['pageUrlSelector']
+	pageUrlSelector: newsConfig['seoul']['pageUrlSelector'],
+	processResult: function(result) {
+		console.log(result);
+	},
+	shouldStopCrawl: function() {
+		return false;
+	}
 }
 
 const cOption = {
 	baseUrl: newsConfig['choongang']['baseUrl'],
 	kind: newsConfig['choongang']['kind']['economy'],
-	pageUrlSelector: newsConfig['choongang']['pageUrlSelector']
+	pageUrlSelector: newsConfig['choongang']['pageUrlSelector'],
+	processResult: function(result) {
+		console.log(result);
+	},
+	shouldStopCrawl: function() {
+		return false;
+	}
 }
 
 const kOption = {
 	baseUrl: newsConfig['kyeonghang']['baseUrl'],
 	kind: newsConfig['kyeonghang']['kind']['society'],
-	pageUrlSelector: newsConfig['kyeonghang']['pageUrlSelector']
+	pageUrlSelector: newsConfig['kyeonghang']['pageUrlSelector'],
+	processResult: function(result) {
+		console.log(result);
+	},
+	shouldStopCrawl: function() {
+		return false;
+	}
 }
 
 const dOption = {
 	baseUrl: newsConfig['donga']['baseUrl'],
 	kind: newsConfig['donga']['kind']['event'],
-	pageUrlSelector: newsConfig['donga']['pageUrlSelector']
+	pageUrlSelector: newsConfig['donga']['pageUrlSelector'],
+	processResult: function(result) {
+		console.log(result);
+	},
+	shouldStopCrawl: function() {
+		return false;
+	}
 }
 
 // const testCrawler = SeoulIlboCrawler(sOption);
